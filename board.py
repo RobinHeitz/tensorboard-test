52e7f8ec7770193be2c7d2a7110356462589ef71
Robin Heitz <robin_rob96@me.com>
2021-08-27 07:29:39 +0200

    deleted again few comments

--- board.py
+++ board.py
@@ -7,10 +7,7 @@ import torchvision.models as models
 from torchvision import datasets
 from tensorboardX import SummaryWriter
 
-resnet18 = models.resnet18(False)
 writer = SummaryWriter('logs/exp-1')
-sample_rate = 44100
-freqs = [262, 294, 330, 349, 392, 440, 440, 440, 440, 440, 440]
 
 for n_iter in range(100):
     dummy_s1 = torch.rand(1)
